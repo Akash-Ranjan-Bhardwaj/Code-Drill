a450bca6988f8f811f4920eaedf7bcac93260ec0
Akash Ranjan Bhardwaj <akashranjanbhardwaj6@gmail.com>
2025-06-07 20:04:47 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://code-drill.onrender.com/api/v1" : "https://code-drill.onrender.com/api/v1",
+  baseURL:"https://code-drill.onrender.com/api/v1",
   withCredentials: true,
 });
